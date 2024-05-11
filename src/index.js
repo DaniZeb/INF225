779e912ebc3a21330c86e8ad6a2f289c3fdd01c6
Javiwas <javiera.osoriom@usm.cl>
2024-05-10 20:27:17 -0400

    Nuevo front

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './AppRouter';  // Asegúrate de importar el componente correcto
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import LoginFunc from './pages/Public/Login/FuncionarioLog';
+import reportWebVitals from './reportWebVitals';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import LoginPac from './pages/Public/Login/PacienteLog';
+import RegisterFunc from './pages/Public/Register/FuncionarioReg';
+import RegisterPac from './pages/Public/Register/PacienteReg';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="login Paciente" element={<LoginPac />} />
+        <Route path="login Funcionario" element={<LoginFunc />} />
+        <Route path="Registro Funcionario" element={<RegisterFunc />} />
+        <Route path="Registro paciente" element={<RegisterPac />} />
+        
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
---
+++ src/pages/Public/Register/PacienteReg.jsx
@@ -0,0 +1,96 @@
+import Navbar from '../../../Components/navbar';
+
+import React, { useEffect, useState } from 'react';
+import { UserRoundPlus, ArrowBigRightDash} from 'lucide-react';
+
+function RegisterPac() {
+  const [navbarBackground, setNavbarBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 100) {
+        setNavbarBackground(true);
+      } else {
+        setNavbarBackground(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  return (
+    <div className=" h-screen w-full betwen " >
+        <nav className={`fixed w-full h-24 shadow-xl ${navbarBackground ? 'bg-white' : 'bg-solid-color'} z-50`}>
+      <Navbar/></nav>
+
+    
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+         
+        <div class="relative w-full max-w-lg m-10 items-center justify-center">
+
+        <div class="absolute top-0 -left-4 w-72 h-72 bg-gray-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+        <div class="absolute top-0 -right-4 w-72 h-72 bg-orange-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+        <div class="absolute -bottom-8 left-20 w-72 h-72 bg-red-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+        
+
+        <div class="w-full bg-orange-200 rounded-lg  dark:border md:mt-0 sm:max-w-md xl:p-0 shadow-xl dark:bg-gray-50 dark:border-gray-50">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-blue-300">
+                  Registrate a tu cuenta de Paciente
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-blue-300">Rut</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-300 dark:border-gray-100 dark:placeholder-gray-100 dark:text-white dark:focus:ring-blue-100 dark:focus:border-blue-200" placeholder="name@company.com" required=""/>
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-blue-300">Contraseña</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-200 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-300 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-100 dark:focus:border-blue-200" required=""/>
+                  </div>
+                  <div class="flex items-center justify-between">
+                      <div class="flex items-start">
+                          <div class="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-200" required=""/>
+                          </div>
+                          <div class="ml-3 text-sm">
+                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                          </div>
+                      </div>
+                      
+                  </div>
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Registrarse</button>
+                  
+              </form>
+          </div>
+      </div>
+      </div>
+       
+
+
+        
+        
+
+        
+      
+        
+ 
+   
+        
+     
+
+     
+      </div>
+
+        
+     
+  
+    <div className='w-10 text-white '></div>
+    </div>
+  );
+}
+
+export default RegisterPac;
